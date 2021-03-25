c52e295e505a874591250e45a47dca3a8c354bba
zahraSoukhtedel <zahra2001so@gmail.com>
2021-03-26 01:13:42 +0430

    hibernate try

--- src/main/java/com/app/model/Notification.java
+++ src/main/java/com/app/model/Notification.java
@@ -1,2 +1,4 @@
+package com.app.model;
+
 public class Notification {
 }
---
+++ src/main/java/com/app/model/User.java
@@ -0,0 +1,64 @@
+package com.app.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column
+    private Long phone;
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+}
